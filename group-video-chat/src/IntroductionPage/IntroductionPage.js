0e4855b0aa5bff45ae7ae175b58b07e7ff3a6ef3
tal1992 <timothylam92@hotmail.co.uk>
2021-07-02 15:48:20 +0100

    Creating Introduction Page

--- group-video-chat/src/IntroductionPage/IntroductionPage.js
+++ group-video-chat/src/IntroductionPage/IntroductionPage.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import logo from '../resources/images/logo.png';
 
 import './IntroductionPage.css';
 
 const IntroductionPage = () => {
-    return (
-        <div>
-            IntroductionPage
+    return <div className='introduction_page_container'>
+        <div className='introduction_page_panel'>
+            <img src={logo} className="introduction_page_image" />
         </div>
-    );
+       </div>
 };
 
 export default IntroductionPage;
