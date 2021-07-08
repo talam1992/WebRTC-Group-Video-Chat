938dc4556c09ec503d0c2bfe91940fbd40200f7f
tal1992 <timothylam92@hotmail.co.uk>
2021-07-08 15:44:22 +0100

    Joining room as user and creating new serverless function

---
+++ group-video-chat-service/functions/room-exists.js
@@ -0,0 +1,42 @@
+exports.handler = function (context, event, callback) {
+    const client = context.getTwilioClient();
+
+    const roomId = event.roomId;
+
+    const response = new Twilio.Response();
+  
+    const headers = {
+      "Access-Control-Allow-Origin": "*", // change this to your client-side URL after deployment
+      "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,OPTIONS",
+      "Access-Control-Allow-Headers": "Content-Type",
+      "Content-Type": "application/json",
+    };
+  
+    response.setHeaders(headers);
+
+    client.video
+        .rooms(roomId)
+        .fetch()
+        .then((room) => {
+        if (room) {
+            response.setBody({
+                roomExists: true,
+                room
+            });
+        } else {
+            response.setBody({
+                roomExists: false,
+            });
+        }
+
+        return callback(null, response);
+    }) .catch(err => {
+        response.setBody({
+            roomExists: false,
+            err
+        });
+
+        return callback(null, response);
+    });
+  };
+  
